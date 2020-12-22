963ea5f16d304e5532599f4086540cab89cb3c18
Akramurridjal Rahman <xerod@outlook.com>
2020-12-22 15:53:47 +0700

    Build packages

--- src/providers/index.ts
+++ src/providers/index.ts
@@ -5,6 +5,7 @@ export * from './google'
 export * from './laravel-jwt'
 export * from './laravel-passport'
 export * from './laravel-sanctum'
+export * from './moka'
 
 export const ProviderAliases = {
   'laravel/jwt': 'laravelJWT',
