33f5c91552fa8687a243a24f634c0ee492f01ae7
Akramurridjal Rahman <xerod@outlook.com>
2020-12-22 15:43:14 +0700

    Add moka as provider

---
+++ src/providers/moka.ts
@@ -0,0 +1,38 @@
+import type { ProviderOptions, ProviderPartialOptions } from '../types'
+import type { Oauth2SchemeOptions } from '../schemes'
+import { assignDefaults, addAuthorize } from '../utils/provider'
+
+export interface MokaProviderOptions
+  extends ProviderOptions,
+    Oauth2SchemeOptions {}
+
+export function moka(
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
+  nuxt: any,
+  strategy: ProviderPartialOptions<MokaProviderOptions>
+): void {
+  const DEFAULTS: typeof strategy = {
+    scheme: 'oauth2',
+    endpoints: {
+      authorization: 'https://api.mokapos.com/oauth/authorize',
+      token: 'https://api.mokapos.com/oauth/token',
+      userInfo: 'https://api.mokapos.com/v1/profile/self',
+      logout: 'https://api.mokapos.com/oauth/revoke'
+    },
+    token: {
+      property: 'access_token',
+      type: 'Bearer',
+      maxAge: 1800
+    },
+    responseType: 'code',
+    grantType: 'authorization_code',
+    accessType: 'offline',
+    redirectUri: 'http://localhost:3000/auth/callback',
+    logoutRedirectUri: '/',
+    scope: ['report', 'customer', 'transaction']
+  }
+
+  assignDefaults(strategy, DEFAULTS)
+
+  addAuthorize(nuxt, strategy)
+}
